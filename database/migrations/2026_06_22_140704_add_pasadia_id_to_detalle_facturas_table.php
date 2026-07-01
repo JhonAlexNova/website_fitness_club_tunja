0ce88ad84729840e263f716728494e67bb62be43
JhonAlexNova <jhonnova19@gmail.com>
2026-07-01 08:48:58 -0500

    correccion migraciones

--- database/migrations/2026_06_22_140704_add_pasadia_id_to_detalle_facturas_table.php
+++ database/migrations/2026_06_22_140704_add_pasadia_id_to_detalle_facturas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPasadiaIdToFacturasTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -21,4 +21,4 @@ class AddPasadiaIdToFacturasTable extends Migration
             $table->dropColumn('pasadia_id');
         });
     }
-}
+};
