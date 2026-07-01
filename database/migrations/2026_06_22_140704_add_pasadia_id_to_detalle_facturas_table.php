b8f4ab4cc72f2b4264af0a5facd1d47a2763f563
JhonAlexNova <jhonnova19@gmail.com>
2026-07-01 09:13:49 -0500

    correccion migraciones facturas

--- database/migrations/2026_06_22_140704_add_pasadia_id_to_detalle_facturas_table.php
+++ database/migrations/2026_06_22_140704_add_pasadia_id_to_detalle_facturas_table.php
@@ -8,15 +8,15 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('facturas', function (Blueprint $table) {
-            $table->unsignedBigInteger('pasadia_id')->nullable()->after('membresia_id');
+        Schema::table('detalle_facturas', function (Blueprint $table) {
+            $table->unsignedBigInteger('pasadia_id')->nullable();
             $table->foreign('pasadia_id')->references('id')->on('pasadias')->onDelete('set null');
         });
     }
 
     public function down()
     {
-        Schema::table('facturas', function (Blueprint $table) {
+        Schema::table('detalle_facturas', function (Blueprint $table) {
             $table->dropForeign(['pasadia_id']);
             $table->dropColumn('pasadia_id');
         });
